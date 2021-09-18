9853c7523481340b51976c5a42258a45945758b4
Diego Romário <diego.romario@outlook.com>
2021-09-18 16:46:47 -0300

    Adding intro step and some options

--- src/ToDo.Bot/Dialogs/MainDialog.cs
+++ src/ToDo.Bot/Dialogs/MainDialog.cs
@@ -3,8 +3,15 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.14.0
 
+using AdaptiveCards;
+using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 namespace ToDo.Bot.Dialogs
@@ -15,13 +22,14 @@ namespace ToDo.Bot.Dialogs
         protected readonly ILogger Logger;
 
         // Dependency injection uses this constructor to instantiate MainDialog
-        public MainDialog(ToDoLUISRecognizer luisRecognizer,ILogger<MainDialog> logger)
+        public MainDialog(ToDoLUISRecognizer luisRecognizer, ILogger<MainDialog> logger)
             : base(nameof(MainDialog))
         {
             _luisRecognizer = luisRecognizer;
             Logger = logger;
 
             AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
             AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
             {
                 IntroStepAsync,
@@ -35,12 +43,42 @@ namespace ToDo.Bot.Dialogs
 
         private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            return null;
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("Which operation would you like to perform?"), cancellationToken);
+
+            List<string> operationList = new List<string> { "Create Task", "View Task", "Delete Task" };
+            // Create card
+            var card = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0))
+            {
+                // Use LINQ to turn the choices into submit actions
+                Actions = operationList.Select(choice => new AdaptiveSubmitAction
+                {
+                    Title = choice,
+                    Data = choice,  // This will be a string
+                }).ToList<AdaptiveAction>(),
+            };
+            // Prompt
+            return await stepContext.PromptAsync(nameof(ChoicePrompt), new PromptOptions
+            {
+                Prompt = (Activity)MessageFactory.Attachment(new Attachment
+                {
+                    ContentType = AdaptiveCard.ContentType,
+                    // Convert the AdaptiveCard to a JObject
+                    Content = JObject.FromObject(card),
+                }),
+                Choices = ChoiceFactory.ToChoices(operationList),
+                // Don't render the choices outside the card
+                Style = ListStyle.None,
+            },
+                cancellationToken);
         }
 
         private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            return null;
+            stepContext.Values["Operation"] = ((FoundChoice)stepContext.Result).Value;
+            string operation = (string)stepContext.Values["Operation"];
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("You have selected - " + operation), cancellationToken);
+
+            return await stepContext.NextAsync(null, cancellationToken);
         }
         private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
